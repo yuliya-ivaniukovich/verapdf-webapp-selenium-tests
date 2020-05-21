83b53c8a424b53b450ed6deb4999a4dcc56da96a
yuliya.ivaniukovich <yuliya.ivaniukovich@duallab.com>
2020-05-21 17:53:32 +0300

    Fix running using maven and setup drivermanager to download chromedriver

--- src/test/java/com/verapdf/selenium/pages/BeforeTest.java
+++ src/test/java/com/verapdf/selenium/pages/BeforeTest.java
@@ -1,19 +1,26 @@
 package com.verapdf.selenium.pages;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 import java.util.concurrent.TimeUnit;
 
 public class BeforeTest {
-    public WebDriver driver = new ChromeDriver();
+    public WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+    }
 
     @BeforeMethod
     public void beforeStart() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Documents\\verapdf-webapp-selenium-tests\\chromedriver.exe");
+        driver = new ChromeDriver();
         driver.get("https://verapdf.duallab.com/demo/new-job/files");
         driver.manage().window().setSize(new Dimension(1044, 788));
         driver.manage().timeouts().pageLoadTimeout(60 * 3, TimeUnit.SECONDS);
